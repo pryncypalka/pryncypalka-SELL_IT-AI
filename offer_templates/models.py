4451dde6fe43d807874292f5499d82b44280e119
Jacek Kamiński <jacus502@gmail.com>
2024-10-14 19:41:35 +0200

    init offer_templates app

---
+++ offer_templates/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from products.models import Item  
+from django.utils import timezone
+from django.conf import settings
+
+class Template(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)  
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.TextField(max_length=3000, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
