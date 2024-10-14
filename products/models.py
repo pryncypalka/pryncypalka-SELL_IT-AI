ffed7eee78010b2a3a27daeb5dac05c13e38268e
Jacek Kamiński <jacus502@gmail.com>
2024-10-14 19:44:14 +0200

    init products app

---
+++ products/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.category_name
+    
+
+class Subcategory(models.Model):
+    subcategory_name = models.CharField(max_length=255, null=False)
+
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.subcategory_name
+
+
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=255, null=False)
+
+    subcategory = models.ForeignKey('Subcategory', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.item_name
