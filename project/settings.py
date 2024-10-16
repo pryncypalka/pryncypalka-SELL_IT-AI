36a66af84d32e350dfe691921c6c0721c1a135c4
Jacek Kamiński <jacus502@gmail.com>
2024-10-16 23:55:08 +0200

    init database in postgres

--- project/settings.py
+++ project/settings.py
@@ -90,10 +90,21 @@ INTERNAL_IPS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Ustawienie silnika na PostgreSQL
+        'NAME': 'SELLIT_AI_DB',               # Nazwa bazy danych
+        'USER': 'postgres',                    # Nazwa użytkownika bazy danych
+        'PASSWORD': 'password',                # Hasło użytkownika bazy danych
+        'HOST': 'localhost',                        # Adres hosta, np. 'localhost' dla lokalnego serwera
+        'PORT': '5433',                             # Port PostgreSQL, domyślnie 5432
     }
 }
 
