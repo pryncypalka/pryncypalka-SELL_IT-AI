eeca93751d375b23d8676dfa8a8b9727aaed1f57
Jacek Kamiński <jacus502@gmail.com>
2024-12-09 21:40:31 +0100

    comented unused offer

--- offers/admin.py
+++ offers/admin.py
@@ -1,86 +1,86 @@
-from django.contrib import admin
-from .models import Offer, Photo, Category
-from .widgets import CategorySelectWidget, DescriptionWidget
-import openai
-from django.urls import path
-from django.http import JsonResponse  
-from project import settings
-openai.api_key = settings.OPENAI_API_KEY
+# from django.contrib import admin
+# from .models import Offer, Photo, Category
+# from .widgets import CategorySelectWidget, DescriptionWidget
+# import openai
+# from django.urls import path
+# from django.http import JsonResponse  
+# from project import settings
+# openai.api_key = settings.OPENAI_API_KEY
 
 
-class PhotoInline(admin.TabularInline):
-    model = Photo
-    extra = 1 
-    readonly_fields = ['image_tag']
+# class PhotoInline(admin.TabularInline):
+#     model = Photo
+#     extra = 1 
+#     readonly_fields = ['image_tag']
     
-    @admin.display(description='Preview')
-    def image_tag(self, obj):
-        if obj:
-            return obj.image_tag()
-        return "No Image"
+#     @admin.display(description='Preview')
+#     def image_tag(self, obj):
+#         if obj:
+#             return obj.image_tag()
+#         return "No Image"
 
-class OfferAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
-    inlines = [PhotoInline]
-    fields = ['title', 'category', 'description', 'price']
-    raw_id_fields = ['category']
+# class OfferAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'description')
+#     inlines = [PhotoInline]
+#     fields = ['title', 'category', 'description', 'price']
+#     raw_id_fields = ['category']
     
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path('generate-description/', self.generate_description, name='generate-description'),
-        ]
-        return custom_urls + urls
+#     def get_urls(self):
+#         urls = super().get_urls()
+#         custom_urls = [
+#             path('generate-description/', self.generate_description, name='generate-description'),
+#         ]
+#         return custom_urls + urls
     
-    def generate_description(self, request):
-        try:
-            title = request.GET.get('title', '')
-            category = request.GET.get('category', '')
+#     def generate_description(self, request):
+#         try:
+#             title = request.GET.get('title', '')
+#             category = request.GET.get('category', '')
             
-            prompt = f"""Napisz profesjonalny opis oferty sprzedaży dla produktu:
-            Tytuł: {title}
-            Kategoria: {category}
+#             prompt = f"""Napisz profesjonalny opis oferty sprzedaży dla produktu:
+#             Tytuł: {title}
+#             Kategoria: {category}
             
-            Opis powinien być:
-            - Szczegółowy ale zwięzły
-            - Profesjonalny
-            - Zawierać najważniejsze cechy produktu
-            - Być napisany w języku polskim
-            """
-            response = openai.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": prompt}],
-                max_tokens=500,
-                temperature=0.7
-            )
+#             Opis powinien być:
+#             - Szczegółowy ale zwięzły
+#             - Profesjonalny
+#             - Zawierać najważniejsze cechy produktu
+#             - Być napisany w języku polskim
+#             """
+#             response = openai.chat.completions.create(
+#                 model="gpt-3.5-turbo",
+#                 messages=[{"role": "user", "content": prompt}],
+#                 max_tokens=500,
+#                 temperature=0.7
+#             )
             
             
-            content = response.choices[0].message.content
-            return JsonResponse({'status': 'success', 'content': content})
-        except Exception as e:
-            print(str(e))
-            return JsonResponse({'status': 'error', 'message': str(e)})
+#             content = response.choices[0].message.content
+#             return JsonResponse({'status': 'success', 'content': content})
+#         except Exception as e:
+#             print(str(e))
+#             return JsonResponse({'status': 'error', 'message': str(e)})
         
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'description':
-            kwargs['widget'] = DescriptionWidget()
-        return super().formfield_for_dbfield(db_field, **kwargs)   
+#     def formfield_for_dbfield(self, db_field, **kwargs):
+#         if db_field.name == 'description':
+#             kwargs['widget'] = DescriptionWidget()
+#         return super().formfield_for_dbfield(db_field, **kwargs)   
     
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "category":
-            kwargs['widget'] = CategorySelectWidget()
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+#     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+#         if db_field.name == "category":
+#             kwargs['widget'] = CategorySelectWidget()
+#         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-    def save_model(self, request, obj, form, change):
-        if not change or obj.user is None:
-            obj.user = request.user
-        super().save_model(request, obj, form, change)
+#     def save_model(self, request, obj, form, change):
+#         if not change or obj.user is None:
+#             obj.user = request.user
+#         super().save_model(request, obj, form, change)
 
-admin.site.register(Offer, OfferAdmin)
+# admin.site.register(Offer, OfferAdmin)
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category_name',)  
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('category_name',)  
 
 
--- offers/models.py
+++ offers/models.py
@@ -1,77 +1,77 @@
-from django.db import models
-from django.utils import timezone
-from django.conf import settings
-from django.utils.html import mark_safe
+# from django.db import models
+# from django.utils import timezone
+# from django.conf import settings
+# from django.utils.html import mark_safe
 
-import os
-import uuid
+# import os
+# import uuid
 
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=255, null=False)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
-    image = models.ImageField(upload_to='images/', null=True, blank=True)  
+# class Category(models.Model):
+#     category_name = models.CharField(max_length=255, null=False)
+#     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+#     image = models.ImageField(upload_to='images/', null=True, blank=True)  
 
-    def __str__(self):
-        if self.parent:
-            return f"{self.category_name} (Subcategory of {self.parent.category_name})"
-        return self.category_name
+#     def __str__(self):
+#         if self.parent:
+#             return f"{self.category_name} (Subcategory of {self.parent.category_name})"
+#         return self.category_name
     
-    def get_subcategories(self):
-        """
-        Recursive method to retrieve all subcategories.
-        """
-        subcategories = []
-        for subcategory in self.subcategories.all():
-            subcategories.append({
-                'id': subcategory.id,
-                'name': subcategory.category_name,
-                'image': subcategory.image.url if subcategory.image else None,
-                'subcategories': subcategory.get_subcategories()
-            })
-        return subcategories
+#     def get_subcategories(self):
+#         """
+#         Recursive method to retrieve all subcategories.
+#         """
+#         subcategories = []
+#         for subcategory in self.subcategories.all():
+#             subcategories.append({
+#                 'id': subcategory.id,
+#                 'name': subcategory.category_name,
+#                 'image': subcategory.image.url if subcategory.image else None,
+#                 'subcategories': subcategory.get_subcategories()
+#             })
+#         return subcategories
 
-    class Meta:
-        verbose_name_plural = "Categories"
-
-
+#     class Meta:
+#         verbose_name_plural = "Categories"
 
 
-def get_file_path(instance, filename):
-    ext = filename.split('.')[-1]
-    filename = f"{uuid.uuid4()}.{ext}"
-    # Dodaje podkatalog z ID oferty
-    return os.path.join(f'offers/{instance.offer.id}', filename)
 
 
-class Offer(models.Model):
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-
-    created_at = models.DateTimeField(default=timezone.now)
-
-    title = models.CharField(max_length=255, null=False)
-
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    description = models.TextField(max_length=3000, null=True)
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Photo(models.Model):
-    photo = models.ImageField(upload_to=get_file_path, null=True, blank=True)  
+# def get_file_path(instance, filename):
+#     ext = filename.split('.')[-1]
+#     filename = f"{uuid.uuid4()}.{ext}"
+#     # Dodaje podkatalog z ID oferty
+#     return os.path.join(f'offers/{instance.offer.id}', filename)
 
 
-    offer = models.ForeignKey('Offer', on_delete=models.CASCADE)
+# class Offer(models.Model):
+#     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
 
-    def __str__(self):
-        return self.offer.title
+#     created_at = models.DateTimeField(default=timezone.now)
+
+#     title = models.CharField(max_length=255, null=False)
+
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+#     description = models.TextField(max_length=3000, null=True)
+
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.title
+
+# class Photo(models.Model):
+#     photo = models.ImageField(upload_to=get_file_path, null=True, blank=True)  
+
+
+#     offer = models.ForeignKey('Offer', on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.offer.title
     
-    def image_tag(self):
-        if self.photo:
-            return mark_safe(
-                f'<img src="{self.photo.url}" style="width: 100px; height: 100px; object-fit: cover; border-radius: 5px;" />'
-            )
-        return "No Image"
+#     def image_tag(self):
+#         if self.photo:
+#             return mark_safe(
+#                 f'<img src="{self.photo.url}" style="width: 100px; height: 100px; object-fit: cover; border-radius: 5px;" />'
+#             )
+#         return "No Image"
